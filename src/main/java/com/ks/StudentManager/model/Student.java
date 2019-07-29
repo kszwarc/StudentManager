ccaf7cd3ddaa92922abeb747287d43499315f9d0
kszwarc <krzysztof@szwarc.net.pl>
2019-07-29 14:59:56 +0200

    Test added

--- src/main/java/com/ks/StudentManager/model/Student.java
+++ src/main/java/com/ks/StudentManager/model/Student.java
@@ -3,6 +3,9 @@ package com.ks.StudentManager.model;
 import lombok.Data;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Data
 @Entity
@@ -11,8 +14,15 @@ public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotNull
+    @Size(min = 1, max = 50)
     private String firstName;
+    @NotNull
+    @Size(min = 1, max = 50)
     private String lastName;
+    @NotNull
+    @Email
+    @Size(min = 1, max = 50)
     private String email;
 
     public Student(String firstName, String lastName, String email) {
