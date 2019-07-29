ccb629512c0f480ac325ffe58407f36cdcaec8a0
kszwarc <krzysztof@szwarc.net.pl>
2019-07-29 09:21:18 +0200

    Update and delete option for Student added

--- src/main/java/com/ks/StudentManager/controller/StudentController.java
+++ src/main/java/com/ks/StudentManager/controller/StudentController.java
@@ -3,12 +3,16 @@ package com.ks.StudentManager.controller;
 import com.ks.StudentManager.model.Student;
 import com.ks.StudentManager.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/")
+@RequestMapping("/students")
 public class StudentController {
     private final StudentRepository studentRepository;
 
@@ -17,13 +21,28 @@ public class StudentController {
         this.studentRepository = studentRepository;
     }
 
-    @GetMapping("students")
-    public List<Student> getStudents () {
+    @GetMapping
+    public List<Student> getSAll () {
         return studentRepository.findAll();
     }
 
-    @PostMapping("students")
-    public Student addBook(@RequestBody Student student) {
+    @PostMapping
+    public Student add(@RequestBody Student student) {
         return studentRepository.save(student);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> update(@RequestBody Student student, @PathVariable long id) {
+        Optional<Student> studentOptional = studentRepository.findById(id);
+        if (!studentOptional.isPresent())
+            return ResponseEntity.notFound().build();
+        student.setId(id);
+        studentRepository.save(student);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping(value="/{id}")
+    public void delete(@PathVariable("id") Long id) {
+        studentRepository.deleteById(id);
     }
 }
--- src/main/java/com/ks/StudentManager/model/Student.java
+++ src/main/java/com/ks/StudentManager/model/Student.java
@@ -22,6 +22,5 @@ public class Student {
     }
 
     public Student() {
-
     }
 }
