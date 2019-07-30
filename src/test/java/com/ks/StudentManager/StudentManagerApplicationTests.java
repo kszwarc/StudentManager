ac819c7ce13a8f676516d70fac1c55c1885fc92a
kszwarc <krzysztof@szwarc.net.pl>
2019-07-30 08:42:31 +0200

    Test added

--- src/test/java/com/ks/StudentManager/StudentManagerApplicationTests.java
+++ src/test/java/com/ks/StudentManager/StudentManagerApplicationTests.java
@@ -1,27 +1,39 @@
 package com.ks.StudentManager;
 
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-
-import java.io.IOException;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@WebAppConfiguration
 public class StudentManagerApplicationTests {
 
+    @Autowired
+    WebApplicationContext webApplicationContext;
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() throws Exception {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
 	@Test
-	public void contextLoads() {
+	public void contextLoads() throws Exception {
+        mockMvc.perform( MockMvcRequestBuilders
+                .get("/students")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().is(204));
 	}
 
 }
